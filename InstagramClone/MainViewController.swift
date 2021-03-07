6e091753525231e634740b3463e8ff3ac06c57a3
ChoiSeongRyoung <sholyspirit2@gmail.com>
2021-03-07 15:48:46 +0900

    탭바 스토리보드 분리

---
+++ InstagramClone/MainViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  InstagramClone
+//
+//  Created by holyspirit on 2021/03/01.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ InstagramClone/View/Tab Bar/HomeTabViewController.swift
@@ -0,0 +1,29 @@
+//
+//  HomeTabViewController.swift
+//  InstagramClone
+//
+//  Created by holyspirit on 2021/03/07.
+//
+
+import UIKit
+
+class HomeTabViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
