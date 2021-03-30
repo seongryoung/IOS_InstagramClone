0fa3f6c999173c4de7f86f7fe16b4ee57a177318
choi seongryoung <sholyspirit2@naver.com>
2021-03-30 15:38:54 +0900

    sort file inspector

--- InstagramClone/AppLifeCycle/AppDelegate.swift
+++ InstagramClone/AppLifeCycle/AppDelegate.swift
@@ -16,18 +16,14 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         self.window = UIWindow(frame: UIScreen.main.bounds)
-        
         // 스토리보드 인스턴스
         let mainStoryBoard = UIStoryboard(name: "MainViewController", bundle: nil)
         // 뷰 컨트롤러 인스턴스
         let mavigationController = mainStoryBoard.instantiateViewController(withIdentifier: "MainViewController")
-        
         // 윈도우의 루트 뷰 컨트롤러 설정
         self.window?.rootViewController = mavigationController
-        
         // 이제 화면에 보여주자.
         self.window?.makeKeyAndVisible()
-        
         
         return true
     }
